021749fe9d5d49c69044af50977f441a62d550a8
KRITIK <101867323+KRITIK8766@users.noreply.github.com>
2022-06-09 23:55:55 +0530

    Update private.py

--- handlers/private.py
+++ handlers/private.py
@@ -14,7 +14,7 @@ if "@" in master_user:
 @Client.on_message(command("start") & filters.private & ~filters.edited)
 async def start_(client: Client, message: Message):
     await message.reply_photo(
-        photo=f"https://telegra.ph/file/72158285463afef8d1044.jpg",
+        photo=f"https://telegra.ph/file/8439f86461d48702d2af2.mp4",
         caption=f"""**𝐓𝐡𝐢𝐬 𝐈𝐬 𝐀𝐝𝐯𝐚𝐧𝐜𝐞 𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦 𝐌𝐮𝐬𝐢𝐜 𝐁𝐨𝐭 𝐑𝐮𝐧 𝐎𝐧 𝐏𝐫𝐢𝐯𝐚𝐭𝐞  𝐕𝐩𝐬  𝐒𝐞𝐫𝐯𝐞𝐫  𝐅𝐞𝐞𝐥 ️ 𝐇𝐢𝐠𝐡 𝐐𝐮𝐚𝐥𝐢𝐭𝐲 𝐌𝐮𝐬𝐢𝐜  𝐈𝐧 𝐕𝐜**""",
     reply_markup=InlineKeyboardMarkup(
             [
@@ -25,11 +25,8 @@ async def start_(client: Client, message: Message):
                 [
                     InlineKeyboardButton(
                         " ✚ 𝘼𝘿𝘿 𝙈𝙀 𝙏𝙊 𝙔𝙊𝙐𝙍 𝙂𝙍𝙊𝙐𝙋 ✚ ", url=f"t.me/HKRMUSICBOT?startgroup=new")
-               ], 
-                [
-                    InlineKeyboardButton(
-                        " are le le repo ", url=f"https://github.com/LEGENDARY-OS/MUSIC-BOT")
                ]
+             
                
                 
            ]
@@ -39,7 +36,7 @@ async def start_(client: Client, message: Message):
 @Client.on_message(command(["repo"]) & filters.group & ~filters.edited)
 async def help(client: Client, message: Message):
     await message.reply_photo(
-        photo=f"https://telegra.ph/file/72158285463afef8d1044.jpg",
+        photo=f"https://telegra.ph/file/8439f86461d48702d2af2.mp4",
         caption=f"""""",
         reply_markup=InlineKeyboardMarkup(
             [
